4569ac8d347bdd1fa4958cce5120f079609441f3
Roy Brabson <brabson@alumni.duke.edu>
2023-07-20 12:56:19 -0400

    create go economy structure

--- pkg/economy/economy.go
+++ pkg/economy/economy.go
@@ -4,10 +4,10 @@ import "time"
 
 // Economy contains all the credits for members on different servers.
 type Economy struct {
-	ID           string                  `json:"_id"`
-	Global       Global                  `json:"global"`
-	Guilds       map[string]Guild        `json:"guilds"`
-	GuildMembers map[string]GuildMembers `json:"guild_members"`
+	ID           string                  `json:"_id" bson:"_id"`
+	Global       Global                  `json:"global" bson:"global"`
+	Guilds       map[string]Guild        `json:"guilds" bson:"guilds"`
+	GuildMembers map[string]GuildMembers `json:"guild_members" bson:"guild_members"`
 }
 
 type Global struct {
@@ -15,19 +15,19 @@ type Global struct {
 }
 
 type Guild struct {
-	ID             string `json:"_id"`
-	BankName       string `json:"bank_name"`
-	Currency       string `json:"currency"`
-	DefaultBalance int    `json:"default_balance"`
+	ID             string `json:"_id" bson:"_id"`
+	BankName       string `json:"bank_name" bson:"bank_name"`
+	Currency       string `json:"currency" bson:"currency"`
+	DefaultBalance int    `json:"default_balance" bson:"default_balance"`
 }
 
 type GuildMembers struct {
-	ID      string            `json:"_id"`
-	Members map[string]Member `json:"members"`
+	ID      string            `json:"_id" bson:"_id"`
+	Members map[string]Member `json:"members" bson:"members"`
 }
 type Member struct {
-	ID        string    `json:"_id"`
-	Balance   int       `json:"balance"`
-	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name"`
+	ID        string    `json:"_id" bson:"_id"`
+	Balance   int       `json:"balance" bson:"balance"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	Name      string    `json:"name" bson:"name"`
 }
