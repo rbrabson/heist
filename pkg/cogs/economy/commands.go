59901508f620ca66ef0d11583fc13ef2bc4497b1
Roy Brabson <brabson@alumni.duke.edu>
2023-08-04 22:59:04 -0400

    Spelling mistake

--- pkg/cogs/economy/commands.go
+++ pkg/cogs/economy/commands.go
@@ -19,7 +19,7 @@ var (
 	commands = []*discordgo.ApplicationCommand{
 		{
 			Name:        "bank",
-			Description: "Commands used to interact with the economy for this server. Restricgted to admins.",
+			Description: "Commands used to interact with the economy for this server. Restricted to admins.",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Name:        "set",
